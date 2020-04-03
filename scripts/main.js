9407a3090cdf417edcb290779a3db2dc98ff4216
Kushagra Agarwal <kushagracool490@gmail.com>
2020-04-03 23:13:11 +0530

    Added a bit of JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'The Beatles';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/th.png') {
+      myImage.setAttribute ('src','images/download.png');
+    } else {
+      myImage.setAttribute ('src','images/th.png');
+    }
+}
